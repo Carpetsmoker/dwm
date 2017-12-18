8edbd71d97cc9c850feb8cbd6412bc53c475eee9
Martin Tournoij <martin@arp242.net>
2017-12-18 10:45:39 +0000

    Add selfrestart patch with modifications
    Modified to make it work in case of recompiles, and some other small
    fixes.
    
    https://dwm.suckless.org/patches/selfrestart/

--- util.h
+++ util.h
@@ -4,5 +4,14 @@
 #define MIN(A, B)               ((A) < (B) ? (A) : (B))
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
 
+typedef union {
+	int i;
+	unsigned int ui;
+	float f;
+	const void *v;
+} Arg;
+
 void die(const char *fmt, ...);
 void *ecalloc(size_t nmemb, size_t size);
+
+void restart(const Arg *arg);
